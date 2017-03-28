d6de3e787b51caae4cda5125b8ded0042e77e71e
Chris Kanich <kaytwo@gmail.com>
2017-03-28 13:56:26 -0500

    test travis

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python tester.py --file test_data.txt --loss .00 --delay 0.01
+python tester.py --file test_data.txt --loss .00 --delay 0.1
+python tester.py --file test_data.txt --loss .00 --delay 0.5
+python tester.py --file test_data.txt --loss .05 --delay 0.1
+python tester.py --file test_data.txt --loss .15 --delay 0.1
+python tester.py --file test_data.txt --loss .25 --delay 0.1
+python tester.py --file test_data.txt --loss .35 --delay 0.1
